d1abece6dc1d1f2781a8cc232c052ab30bdb72da
Alberto Russo <alberto9449@gmail.com>
2022-08-22 10:39:48 +0200

    autocompiled + autorestart server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,14 @@
-type Name = string;
-const name: Name = "alberto";
-throw new Error("Something went wrong");
-console.log(`Ciao ciao ${name}`);
+import express from "express";
+import "express-async-errors";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send(" prova ricompilazione");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
